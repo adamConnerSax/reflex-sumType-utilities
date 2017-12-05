a0a73c80df4c4265223ca9ea38a8ceaed8b3ad52
adamConnerSax <adam_conner_sax@yahoo.com>
2017-12-05 11:50:17 -0500

    Updated to use working factorDyn.  Change factorDyn' to factorDynGeneric.

--- src/Reflex/Dynamic/FactorDyn.hs
+++ src/Reflex/Dynamic/FactorDyn.hs
@@ -8,28 +8,65 @@
 module Reflex.Dynamic.FactorDyn where
 
 import Reflex (Reflex, MonadHold, Dynamic, Event, fmapMaybe, fforMaybe, never, switch, sample, current, updated, push, holdDyn, ffor, hold)
+import Reflex (EitherTag (..), eitherToDSum, coerceDynamic, takeWhileE)
 import Control.Monad.Fix (MonadFix)
 import Data.Maybe (isJust)
 import Data.Dependent.Sum (DSum ((:=>)))
 import Data.GADT.Compare (GEq, (:~:) (Refl), geq)
 import Data.Functor.Compose (Compose (Compose), getCompose)
+import Data.Functor.Identity (Identity)
 
 import Generics.SOP.Distribute (distributeI_NP)
 import Generics.SOP.DMapUtilities (dSumToNS, nsToDSum, nsOfnpUnCompose, FunctorWrapTypeListOfLists)
 import Generics.SOP (Generic, Code, from, unSOP, SOP (SOP), to, hmap, hcmap, Proxy (Proxy), I (I), (:.:) (Comp), SListI)--(from, unSOP)
 
 
+-- | Factor a @Dynamic t (Maybe a)@ into a @Dynamic t (Maybe (Dynamic t a))@,
+-- such that the outer 'Dynamic' is updated only when the 'Maybe''s constructor
+-- chages from 'Nothing' to 'Just' or vice-versa.  Whenever the constructor
+-- becomes 'Just', an inner 'Dynamic' will be provided, whose value will track
+-- the 'a' inside the 'Just'; when the constructor becomes 'Nothing', the
+-- existing inner 'Dynamic' will become constant, and will not change when the
+-- outer constructor changes back to 'Nothing'.
+{-
 maybeDyn :: forall t a m. (Reflex t, MonadFix m, MonadHold t m) => Dynamic t (Maybe a) -> m (Dynamic t (Maybe (Dynamic t a)))
-maybeDyn d = do
-  ma <- sample $ current d
-  let inner :: forall m'. (MonadFix m', MonadHold t m') => a -> m' (Dynamic t a)
-      inner a = holdDyn a . fmapMaybe id =<< takeWhileE isJust (updated d)
-  mInner0 :: Maybe (Dynamic t a) <- mapM inner ma
-  rec result <- holdDyn mInner0 $ flip push (updated d) $ \new -> do
-        old <- sample $ current d
-        if isJust old == isJust new then return Nothing else Just <$> mapM inner new
-  return result
+maybeDyn = fmap (fmap unpack) . eitherDyn . fmap pack
+  where pack = \case
+          Nothing -> Left ()
+          Just a -> Right a
+        unpack = \case
+          Left _ -> Nothing
+          Right a -> Just a
 
+eitherDyn :: forall t a b m. (Reflex t, MonadFix m, MonadHold t m) => Dynamic t (Either a b) -> m (Dynamic t (Either (Dynamic t a) (Dynamic t b)))
+eitherDyn = fmap (fmap unpack) . factorDyn . fmap eitherToDSum
+  where unpack :: DSum (EitherTag a b) (Compose (Dynamic t) Identity) -> Either (Dynamic t a) (Dynamic t b)
+        unpack = \case
+          LeftTag :=> Compose a -> Left $ coerceDynamic a
+          RightTag :=> Compose b -> Right $ coerceDynamic b
+-}
+factorDyn :: forall t m k (v :: [*] -> *) . (Reflex t, MonadFix m, MonadHold t m, GEq k) => Dynamic t (DSum k v) -> m (Dynamic t (DSum k (Compose (Dynamic t) v)))
+factorDyn d = do
+  let inner :: forall m' a. (MonadFix m', MonadHold t m') => k a -> v a -> m' (Dynamic t (v a))
+      inner k v0 = holdDyn v0 . fmapMaybe id =<< takeWhileE isJust newVal
+        where newVal = ffor (updated d) $ \(newK :=> newV) -> case newK `geq` k of
+                Just Refl -> Just newV
+                Nothing -> Nothing
+      getInitial = do
+        k0 :=> (v0 :: v a) <- sample $ current d
+        i0 <- inner k0 v0
+        return $ k0 :=> Compose i0
+      update = flip push (updated d) $ \(newKey :=> newVal) -> do
+        (oldKey :=> _) <- sample $ current d
+        case newKey `geq` oldKey of
+          Just Refl -> return Nothing
+          Nothing -> do
+            newInner <- inner newKey newVal
+            return $ Just $ newKey :=> Compose newInner
+  o0 <- getInitial --TODO: Figure out how to get this to run inside something like the first argument to buildDynamic
+  holdDyn o0 update
+
+{-
 takeWhileE :: forall t m a. (Reflex t, MonadFix m, MonadHold t m) => (a -> Bool) -> Event t a -> m (Event t a)
 takeWhileE f e = do
   rec be <- hold e $ fforMaybe e' $ \a -> if f a
@@ -56,11 +93,11 @@ factorDyn d = do
             newInner <- inner newKey newVal
             return $ Just $ newKey :=> Compose newInner
   return result
+-}
 
-
-factorDyn' :: forall t m a b. (Reflex t, MonadFix m, MonadHold t m, Generic a, Generic b, (Code b) ~ FunctorWrapTypeListOfLists (Dynamic t) (Code a))
+factorDynGeneric :: forall t m a b. (Reflex t, MonadFix m, MonadHold t m, Generic a, Generic b, (Code b) ~ FunctorWrapTypeListOfLists (Dynamic t) (Code a))
   => Dynamic t a -> m (Dynamic t b)
-factorDyn' da = do 
+factorDynGeneric da = do 
   let dSumDyn = nsToDSum . unSOP . from <$> da -- Dynamic t (DSum (TypeListTag (Code a)) (NP I xs))
   dSumDyn' <- factorDyn dSumDyn  -- Dynamic t (DSum (TypeListTag (Code a)) (NP I))
   let nsnpDyn = dSumToNS <$> dSumDyn' -- Dynamic t (NS (Compose (Dynamic t) (NP I)) (Code a))
@@ -73,7 +110,7 @@ factorDyn' da = do
 
 -- for example:
 maybeDyn' :: (Reflex t, MonadHold t m, MonadFix m) => Dynamic t (Maybe a) -> m (Dynamic t (Maybe (Dynamic t a)))
-maybeDyn' = factorDyn'
+maybeDyn' = factorDynGeneric
 
-eitherDyn :: (Reflex t, MonadHold t m, MonadFix m) => Dynamic t (Either a b) -> m (Dynamic t (Either (Dynamic t a) (Dynamic t b)))
-eitherDyn = factorDyn'
+eitherDyn' :: (Reflex t, MonadHold t m, MonadFix m) => Dynamic t (Either a b) -> m (Dynamic t (Either (Dynamic t a) (Dynamic t b)))
+eitherDyn' = factorDynGeneric
